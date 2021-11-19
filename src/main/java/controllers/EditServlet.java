81dbff64d2c7f58d656820c54011c17d47ed744d
Goto <gotoshingo@192.168.0.8>
2021-11-19 21:01:56 +0900

    UpdateServlet.javaを作成

--- src/main/java/controllers/EditServlet.java
+++ src/main/java/controllers/EditServlet.java
@@ -13,14 +13,23 @@ import javax.servlet.http.HttpServletResponse;
 import models.Message;
 import utils.DBUtil;
 
+/**
+ * Servlet implementation class EditServlet
+ */
 @WebServlet("/edit")
 public class EditServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
     public EditServlet() {
         super();
     }
 
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
@@ -39,5 +48,4 @@ public class EditServlet extends HttpServlet {
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
         rd.forward(request, response);
     }
-
 }
